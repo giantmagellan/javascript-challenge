755f14e755b6440c1cdd1e94cf3fa55d553d3ed6
giantmagellan <51727942+giantmagellan@users.noreply.github.com>
2020-03-09 22:39:18 -0700

    cleaned

--- static/js/app.js
+++ static/js/app.js
@@ -2,52 +2,21 @@
 var tableData = data;
 console.log(tableData);
 
-// // Referencing the table body
-// var tbody = d3.select("tbody")
-
-// // Loop through UFO Sightings data
-// data.forEach(function(Sightings) {
-//     //console.log(Sightings);
-    
-//     // Use d3 to append one table row 'tr' for each UFO Signting object
-//     var row = tbody.append("tr");
-//     // Object.entries to console.log each UFO Sighting value
-//     Object.entries(Sightings).forEach(function([key, value]) {
-//         console.log(key, value);
-//     });
-//     // d3 to append 1 cell per signting value
-//     //var cell = row.append("td");
-//     //cell.text(value);
-// });
-
-// key up event
-// $('#datetime').on('keyup', function() {
-//     var value = $(this).val();
-//     console.log(value);
-
-// });
-
 buildTable(tableData)
 
-// function searchTable() {
-//     // returns new array
-//     var filterData = data.filter() 
-//     // loops through all values and returns desired value
-//     for (var i = 0; i < data.length; i++) {
-        
-//         var date_time = data[j].datetime
+var button = d3.select("#filter-btn");
 
-//         if (date_time.includes(value)) {
-//             filterData.push(data[i])
-//         }
-//     }
-//     return filterData
-// }
-var button = d3.select("#button");
+button.on("click", function(){
+    var inputElement = d3.select("datetime");
+    console.log(inputElement);
 
-button.on
-var inputValue = inputElement.property("value")
-var filteredData = tableData.filter(date_time =>date_time.datetime === inputValue);
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(date_time => date_time.datetime === inputValue);
+    console.log(filteredData);
+
+});
+
 
 function buildTable(data){
     // querying the table with id 'ufo-table'
@@ -73,22 +42,3 @@ function buildTable(data){
                 }
     table.html(newData);
 }
-
-
-    
-// function buildTable(data){
-//     var table = document.getElementById('myTable')
-//     table.innerHTML = ''
-//     for (var i = 0; i < data.length; i++){
-//         var colname = `name-${i}`
-//         var colage = `age-${i}`
-//         var colbirth = `birth-${i}`
-
-//         var row = `<tr>
-//                         <td>${data[i].name}</td>
-//                         <td>${data[i].age}</td>
-//                         <td>${data[i].birthdate}</td>
-//                    </tr>`
-//         table.innerHTML += row
-//     }
-// }
